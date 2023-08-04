a4e73eb9c76f464fd5cf63ca59ec37e4383e3817
Rusatam1202 <rustamtolliboyev789@gmail.com>
2023-08-04 19:38:45 +0500

    add

--- src/API/api.js
+++ src/API/api.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000'
- 
+const BASE_URL = 'http://localhost:5000';
+  
+
 export const api={
     userRegister:(user)=> axios.post(BASE_URL + '/register',user),
     userLogin:(user)=> axios.post(BASE_URL + '/user/login',user)
