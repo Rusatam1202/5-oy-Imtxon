4d62c29ef28bf2983d6a870c59868e5fc01dd264
Rusatam1202 <rustamtolliboyev789@gmail.com>
2023-08-04 19:18:14 +0500

    add commit

--- src/API/api.js
+++ src/API/api.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000'
-
+ 
 export const api={
     userRegister:(user)=> axios.post(BASE_URL + '/register',user),
     userLogin:(user)=> axios.post(BASE_URL + '/user/login',user)
